2fc3c54dc945cab5a9af770428f90498a9fa1f79
Eric.Leask <16efl@queensu.ca>
2017-03-26 18:04:05 -0400

    F1rst C0mmi7!!!1

---
+++ app/src/main/java/c/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package c.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openSettings(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+    }
+
