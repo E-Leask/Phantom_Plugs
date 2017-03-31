dc52e0d31b1eaea4ad4de5479fe3f3963858ef65
Eric.Leask <16efl@queensu.ca>
2017-03-30 22:45:44 -0400

    7h1rd C0mmi7!!!1(Very tiny update

--- app/src/main/java/c/myapplication/Main2Activity.java
+++ app/src/main/java/c/myapplication/Main2Activity.java
@@ -1,7 +1,9 @@
 package c.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+    public void openMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
